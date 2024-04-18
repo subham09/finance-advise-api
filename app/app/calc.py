2cf4fb2ff942b71b84f0c97268e79e8c8b42725e
subham PR <subhamanand@rocketmail.com>
2024-04-18 20:01:36 +1000

    till 28

---
+++ app/app/calc.py
@@ -0,0 +1,8 @@
+"""
+Calculation functions
+"""
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
---
+++ app/app/tests.py
@@ -0,0 +1,15 @@
+"""
+Sample tests
+"""
+from django.test import SimpleTestCase
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    
+    def test_add(self):
+        res = calc.add(3, 8)
+        self.assertEqual(res, 11)
+
+    def test_subtract(self):
+        res = calc.subtract(11, 5)
+        self.assertEqual(res, 7)
